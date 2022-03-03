db9a1b2c3ddd93d6a241c69b446d1fc020427292
Eric Matlock <ermatlock@gmail.com>
2022-03-03 16:05:29 -0500

    Update webpack to accept scss

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,11 @@ module.exports = {
   "module": {
     "rules": [
       {
-        test: /\.css$/,
+        test: /\.s[ac]ss$/i,
         use: [
-            { loader: 'style-loader' },
-            { loader: 'css-loader' }
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
         ]
     },
       {
