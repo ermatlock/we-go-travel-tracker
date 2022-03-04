34e2421d9ce416dab21660a12eef20db9c40d6ac
Eric Matlock <ermatlock@gmail.com>
2022-03-03 23:42:40 -0500

    Gather user data

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,33 +1,32 @@
-import chai from 'chai';
+import chai from "chai";
 const expect = chai.expect;
 import Traveler from "../src/Traveler";
 
 describe("User", () => {
   let userData1;
   let userData2;
-  let userData3;
   let user1;
   let user2;
 
   beforeEach(() => {
-    userData1 = {
+    user1Data = {
       id: 1,
       name: "Ham Leadbeater",
-      travelerType: "relaxer"
-      }
-    userData2 = {
-        id: 2,
-        name: "Rachael Vaughten",
-        travelerType: "thrill-seeker"
-      }
-    userData3 = {
-        id": 3,
-        name: "Sibby Dawidowitsch",
-        travelerType: "shopper"
-      }
+      travelerType: "relaxer",
+    };
+    user2Data = {
+      id: 2,
+      name: "Rachael Vaughten",
+      travelerType: "thrill-seeker",
+    };
+    user3Data = {
+      id: undefined,
+      name: undefined,
+      travelerType: undefined,
+    };
 
-    user1 = new User(userData1);
-    user2 = new User(userData2);
+    user1 = new User(user1Data);
+    user2 = new User(user2Data);
+    user3 = new User(user3Data);
   });
-
 });
