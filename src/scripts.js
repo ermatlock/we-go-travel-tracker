c172cc74149eb517a0eb967378042ad6e8b9d679
Eric Matlock <ermatlock@gmail.com>
2022-03-06 16:06:49 -0500

    Update promiseAll

--- src/scripts.js
+++ src/scripts.js
@@ -5,19 +5,34 @@
 import './css/base.scss';
 import dayjs from 'dayjs'
 import domUpdates from './dom-updates'
+import Traveler from './Traveler'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import Traveler from './Traveler';
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
-const promiseAll = () => {
-  Promise.all([
-    fetch("http://localhost:3001/api/v1/travelers/")
-    fetch("http://localhost:3001/api/v1/trips")
-    fetch("http://localhost:3001/api/v1/destinations")
-  ]).then((data) => {
+let currentTraveler;
 
+const createTraveler (id, travelers) => {
+  currentTraveler = new Traveler(travelers.find(traveler => traveler.id === id)));
+}
+
+const loadPage = () => {
+  Promise.all([
+    fetch("http://localhost:3001/api/v1/travelers/"),
+    fetch("http://localhost:3001/api/v1/trips"),
+    fetch("http://localhost:3001/api/v1/destinations"),
+  ]).then((data) => {
+    const travelers = data[0];
+    const trips = data[1];
+    const destinations = data[2];
+    const id = 2
+    createTraveler(id, travelers)
   })
-}   
+}
+
+
+window.onload = loadPage;
