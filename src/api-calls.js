dbbc8090057864aeeaf73cc761b103307f3615a0
Eric Matlock <ermatlock@gmail.com>
2022-03-06 14:49:52 -0500

    Create and add fetch and post calls

---
+++ src/api-calls.js
@@ -0,0 +1,27 @@
+import domUpdates from "./domUpdates";
+
+const fetch = (address) => {
+  return fetch(address)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+      // domUpdates.showError(
+      //   "Sorry, we were unable to retrieve your data. Please contact @nikseif to file a complaint"
+      // );
+    });
+};
+
+const post = (address, inputData) => {
+  return fetch(address, {
+    method: "POST",
+    body: JSON.stringify(inputData),
+    headers: { "Content-Type": "application/json" },
+  }).catch((error) => {
+    console.log(error);
+    // domUpdates.showError(
+    //   "Sorry, we were unable to record your data. Please contact @hfaerber to file a complaint"
+    // );
+  });
+};
+
+export {fetch, post}
