a1590d5a68621fba6db31cad7e3c67204c23972d
Eric Matlock <ermatlock@gmail.com>
2022-03-06 12:49:56 -0500

    Import dayjs

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,7 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import dayjs from 'dayjs'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
