8bb14034128ce2d499396bb94427b61dc28e6300
Eric Matlock <ermatlock@gmail.com>
2022-03-06 16:41:56 -0500

    Add greetUser and showError functions

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -1,3 +1,15 @@
-let domUpdates = {}
+
+
+
+let domUpdates = {
+  showError(message) {
+    errorMessage.innerText = message;
+    modal.style.display = "block";
+  },
+
+  greetUser(currentUser) {
+    welcome.innerText = `Welcome, ${currentUser.getFirstName()}`;
+  },
+}
 
 export default domUpdates;
