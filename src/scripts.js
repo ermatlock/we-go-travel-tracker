763962922ecf6bb3de393ae56e1bf1d10cfefe7b
Eric Matlock <ermatlock@gmail.com>
2022-03-06 14:50:40 -0500

    Add promiseAll

--- src/scripts.js
+++ src/scripts.js
@@ -4,9 +4,20 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 import dayjs from 'dayjs'
+import domUpdates from './dom-updates'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+const promiseAll = () => {
+  Promise.all([
+    fetch("http://localhost:3001/api/v1/travelers/")
+    fetch("http://localhost:3001/api/v1/trips")
+    fetch("http://localhost:3001/api/v1/destinations")
+  ]).then((data) => {
+
+  })
+}   
