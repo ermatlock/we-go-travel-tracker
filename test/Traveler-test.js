a6ab7a7e2f52ea331c019a72a5bb6e5635427b34
Eric Matlock <ermatlock@gmail.com>
2022-03-07 13:53:56 -0500

    Prettier, update annual spending test

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -5,15 +5,15 @@ import trips from "./test-data-trips";
 import travelers from "./test-data-travelers";
 import destinations from "./test-data-destinations";
 import Trip from "../src/Trip";
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 let traveler1;
 let traveler2;
 let allTrips;
 let user2Trips;
-let allDestinations
-let userInput
-let today
+let allDestinations;
+let userInput;
+let today;
 
 describe("Traveler", () => {
   beforeEach(() => {
@@ -25,40 +25,40 @@ describe("Traveler", () => {
         userID: 2,
         destinationID: 10,
         travelers: 5,
-        date: '2019/09/27',
+        date: "2019/09/27",
         duration: 13,
-        status: 'approved',
-        suggestedActivities: []
+        status: "approved",
+        suggestedActivities: [],
       },
       {
         id: 100,
         userID: 2,
         destinationID: 6,
         travelers: 6,
-        date: '2020/3/28',
+        date: "2020/3/28",
         duration: 10,
-        status: 'approved',
-        suggestedActivities: []
+        status: "approved",
+        suggestedActivities: [],
       },
       {
         id: 116,
         userID: 2,
         destinationID: 7,
         travelers: 3,
-        date: '2020/04/03',
+        date: "2020/04/03",
         duration: 8,
-        status: 'approved',
-        suggestedActivities: []
+        status: "approved",
+        suggestedActivities: [],
       },
       {
         id: 166,
         userID: 2,
         destinationID: 7,
         travelers: 2,
-        date: '2020/03/05',
+        date: "2020/03/05",
         duration: 6,
-        status: 'approved',
-        suggestedActivities: []
+        status: "approved",
+        suggestedActivities: [],
       },
       {
         id: 203,
@@ -70,12 +70,12 @@ describe("Traveler", () => {
         status: "approved",
         suggestedActivities: [],
       },
-    ]
+    ];
     // userInput = new Trip(2, 1, 2, "2022/03/12", 4)
-    today = dayjs().format('YYYY/MM/DD')
+    today = dayjs().format("YYYY/MM/DD");
 
-    traveler1 = new Traveler(travelers.travelers[1]);
-    traveler2 = new Traveler(travelers.travelers[3]);
+    traveler1 = new Traveler(travelers[1]);
+    traveler2 = new Traveler(travelers[3]);
   });
 
   it("should be a function", () => {
@@ -117,8 +117,8 @@ describe("Traveler", () => {
   });
 
   it("should have today's date", () => {
-    expect(traveler1.todayDate).to.equal(today)
-    expect(traveler2.todayDate).to.equal(today)
+    expect(traveler1.todayDate).to.equal(today);
+    expect(traveler2.todayDate).to.equal(today);
   });
 
   it("should collect all traveler's trip data", () => {
@@ -128,22 +128,10 @@ describe("Traveler", () => {
     expect(traveler2.trips).to.eql([]);
   });
 
-
   it("should get a total of my annual spending", () => {
-    traveler1.getMyAnnualSpending(allTrips, allDestinations);
-    traveler2.getMyAnnualSpending(allTrips, allDestinations);
-    expect(traveler1.trips).to.equal("19308.30");
-    expect(traveler2.trips).to.equal(0);
-
+    const total1 = traveler1.getMyAnnualSpending(allTrips, allDestinations);
+    const total2 = traveler2.getMyAnnualSpending(allTrips, allDestinations);
+    expect(total1).to.equal(19308.3);
+    expect(total2).to.equal(0);
+  });
 });
-
-// it("should take a trip request", () => {
-
-//   traveler1.makeTripRequest(1, 2, "2022/03/12", 4);
-//   traveler2.makeTripRequest();
-//   expect(traveler1.trips).to.equal();
-//   expect(traveler2.trips).to.equal({});
-
-// });
-
-})
