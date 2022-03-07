6b7baf8d7cdb5ee4ea8dd96403b8eee05de25c5f
Eric Matlock <ermatlock@gmail.com>
2022-03-07 13:53:05 -0500

    Prettier, remove comma at end of array

--- test/test-data-destinations.js
+++ test/test-data-destinations.js
@@ -1,104 +1,103 @@
 // test data for class testing
 const destinations = [
-    {
-      id: 1,
-      destination: "Lima, Peru",
-      estimatedLodgingCostPerDay: 70,
-      estimatedFlightCostPerPerson: 400,
-      image:
-        "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
-      alt: "overview of city buildings with a clear sky",
-    },
-    {
-      id: 2,
-      destination: "Stockholm, Sweden",
-      estimatedLodgingCostPerDay: 100,
-      estimatedFlightCostPerPerson: 780,
-      image:
-        "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-      alt: "city with boats on the water during the day time",
-    },
-    {
-      id: 3,
-      destination: "Sydney, Austrailia",
-      estimatedLodgingCostPerDay: 130,
-      estimatedFlightCostPerPerson: 950,
-      image:
-        "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-      alt: "opera house and city buildings on the water with boats",
-    },
-    {
-      id: 4,
-      destination: "Cartagena, Colombia",
-      estimatedLodgingCostPerDay: 65,
-      estimatedFlightCostPerPerson: 350,
-      image:
-        "https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
-      alt: "boats at a dock during the day time",
-    },
-    {
-      id: 6,
-      destination: "Jakarta, Indonesia",
-      estimatedLodgingCostPerDay: 70,
-      estimatedFlightCostPerPerson: 890,
-      image:
-        "https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-      alt: "lit up city at night",
-    },
-    {
-      id: 7,
-      destination: "Paris, France",
-      estimatedLodgingCostPerDay: 100,
-      estimatedFlightCostPerPerson: 395,
-      image:
-        "https://images.unsplash.com/photo-1524396309943-e03f5249f002?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
-      alt: "city during the day time with eiffel tower",
-    },
-    {
-      id: 10,
-      destination: "Toronto, Canada",
-      estimatedLodgingCostPerDay: 90,
-      estimatedFlightCostPerPerson: 450,
-      image:
-        "https://images.unsplash.com/photo-1535776142635-8fa180c46af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2756&q=80",
-    },
-    {
-      id: 12,
-      destination: "Wellington, New Zealand",
-      estimatedLodgingCostPerDay: 150,
-      estimatedFlightCostPerPerson: 1200,
-      image:
-        "https://images.unsplash.com/photo-1442483221814-59f7d8b22739?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-      alt: "overview of city with buildings, water and trees",
-    },
-    {
-      id: 22,
-      destination: "Rome, Italy",
-      estimatedLodgingCostPerDay: 90,
-      estimatedFlightCostPerPerson: 650,
-      image:
-        "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-      alt: "people standing inside a colosseum during the day",
-    },
-    {
-      id: 25,
-      destination: "New York, New York",
-      estimatedLodgingCostPerDay: 175,
-      estimatedFlightCostPerPerson: 200,
-      image:
-        "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-      alt:
-        "people crossing the street during the day surrounded by tall buildings and advertisements",
-    },
-    {
-      id: 49,
-      destination: "Castries, St Lucia",
-      estimatedLodgingCostPerDay: 650,
-      estimatedFlightCostPerPerson: 90,
-      image:
-        "https://images.unsplash.com/photo-1524478075552-c2763ea171b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80",
-      alt: "aerial photography of rocky mountain under cloudy sky",
-    },
-  ],
+  {
+    id: 1,
+    destination: "Lima, Peru",
+    estimatedLodgingCostPerDay: 70,
+    estimatedFlightCostPerPerson: 400,
+    image:
+      "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+    alt: "overview of city buildings with a clear sky",
+  },
+  {
+    id: 2,
+    destination: "Stockholm, Sweden",
+    estimatedLodgingCostPerDay: 100,
+    estimatedFlightCostPerPerson: 780,
+    image:
+      "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "city with boats on the water during the day time",
+  },
+  {
+    id: 3,
+    destination: "Sydney, Austrailia",
+    estimatedLodgingCostPerDay: 130,
+    estimatedFlightCostPerPerson: 950,
+    image:
+      "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "opera house and city buildings on the water with boats",
+  },
+  {
+    id: 4,
+    destination: "Cartagena, Colombia",
+    estimatedLodgingCostPerDay: 65,
+    estimatedFlightCostPerPerson: 350,
+    image:
+      "https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+    alt: "boats at a dock during the day time",
+  },
+  {
+    id: 6,
+    destination: "Jakarta, Indonesia",
+    estimatedLodgingCostPerDay: 70,
+    estimatedFlightCostPerPerson: 890,
+    image:
+      "https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "lit up city at night",
+  },
+  {
+    id: 7,
+    destination: "Paris, France",
+    estimatedLodgingCostPerDay: 100,
+    estimatedFlightCostPerPerson: 395,
+    image:
+      "https://images.unsplash.com/photo-1524396309943-e03f5249f002?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
+    alt: "city during the day time with eiffel tower",
+  },
+  {
+    id: 10,
+    destination: "Toronto, Canada",
+    estimatedLodgingCostPerDay: 90,
+    estimatedFlightCostPerPerson: 450,
+    image:
+      "https://images.unsplash.com/photo-1535776142635-8fa180c46af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2756&q=80",
+  },
+  {
+    id: 12,
+    destination: "Wellington, New Zealand",
+    estimatedLodgingCostPerDay: 150,
+    estimatedFlightCostPerPerson: 1200,
+    image:
+      "https://images.unsplash.com/photo-1442483221814-59f7d8b22739?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "overview of city with buildings, water and trees",
+  },
+  {
+    id: 22,
+    destination: "Rome, Italy",
+    estimatedLodgingCostPerDay: 90,
+    estimatedFlightCostPerPerson: 650,
+    image:
+      "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "people standing inside a colosseum during the day",
+  },
+  {
+    id: 25,
+    destination: "New York, New York",
+    estimatedLodgingCostPerDay: 175,
+    estimatedFlightCostPerPerson: 200,
+    image:
+      "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "people crossing the street during the day surrounded by tall buildings and advertisements",
+  },
+  {
+    id: 49,
+    destination: "Castries, St Lucia",
+    estimatedLodgingCostPerDay: 650,
+    estimatedFlightCostPerPerson: 90,
+    image:
+      "https://images.unsplash.com/photo-1524478075552-c2763ea171b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80",
+    alt: "aerial photography of rocky mountain under cloudy sky",
+  },
+];
 
-export default destinations
+export default destinations;
