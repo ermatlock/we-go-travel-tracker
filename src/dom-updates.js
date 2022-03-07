4e4bcbdedb0aa829e2ad2a6fd3d5168402e7cfac
Eric Matlock <ermatlock@gmail.com>
2022-03-06 20:51:23 -0500

    Add error query selectors/event listeners

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -1,15 +1,33 @@
+/*~~~~~~~~~~~~~~~QUERY SELECTORS~~~~~~~~~~~~~~~~~*/
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+const errorMessage = document.getElementById("errorMessage");
 
+/*~~~~~~~~~~~~~~~EVENT LISTENERS~~~~~~~~~~~~~~~~~*/
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
-
-let domUpdates = {
+const domUpdates = {
   showError(message) {
     errorMessage.innerText = message;
     modal.style.display = "block";
   },
 
-  greetUser(currentUser) {
-    welcome.innerText = `Welcome, ${currentUser.getFirstName()}`;
+  greetUser(currentTraveler) {
+    welcome.innerText = `Welcome, ${currentTraveler.getFirstName()}`;
+
   },
-}
+  
+
+  // updateTravelerScr(currentTraveler) {
+  //   currentTraveler.
+  // }
+};
 
 export default domUpdates;
