473a1a56051084075468250c99e326c1701f4346
Eric Matlock <ermatlock@gmail.com>
2022-03-07 20:48:26 -0500

    Add error functions to catch

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,24 +1,26 @@
+import domUpdates from "./dom-updates";
+
 const getData = (address) => {
   return fetch(`http://localhost:3001/api/v1/${address}`)
     .then((response) => response.json())
     .catch((error) => {
       console.log(error);
-      // domUpdates.showError(
-      //   "Sorry, we were unable to retrieve your data. Please contact @nikseif to file a complaint"
-      // );
+      domUpdates.showError(
+        "Sorry, we were unable to retrieve your data. Please contact @nikseif to file a complaint"
+      );
     });
 };
 
 const postData = (address, inputData) => {
-  return fetch(address, {
+  return fetch(`http://localhost:3001/api/v1/${address}`, {
     method: "POST",
     body: JSON.stringify(inputData),
     headers: { "Content-Type": "application/json" },
   }).catch((error) => {
     console.log(error);
-    // domUpdates.showError(
-    //   "Sorry, we were unable to record your data. Please contact @hfaerber to file a complaint"
-    // );
+    domUpdates.showError(
+      "Sorry, we were unable to record your data. Please contact @hfaerber to file a complaint"
+    );
   });
 };
 
